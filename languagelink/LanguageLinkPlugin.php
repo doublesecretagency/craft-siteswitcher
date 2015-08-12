4962439ee8dc1c59da0044edaff9c404df76cd73
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2015-08-11 17:15:04 -0700

    (v1.1.0) Added `ll` Twig function shortcut

--- languagelink/LanguageLinkPlugin.php
+++ languagelink/LanguageLinkPlugin.php
@@ -4,25 +4,31 @@ namespace Craft;
 class LanguageLinkPlugin extends BasePlugin
 {
 
-    public function getName()
-    {
-        return 'Language Link';
-    }
+	public function getName()
+	{
+		return 'Language Link';
+	}
 
-    public function getVersion()
-    {
-        return '1.0.1';
-    }
+	public function getVersion()
+	{
+		return '1.1.0';
+	}
 
-    public function getDeveloper()
-    {
-        return 'Double Secret Agency';
-    }
+	public function getDeveloper()
+	{
+		return 'Double Secret Agency';
+	}
 
-    public function getDeveloperUrl()
-    {
-        return 'https://github.com/lindseydiloreto/craft-languagelink';
-        //return 'http://doublesecretagency.com';
-    }
-    
+	public function getDeveloperUrl()
+	{
+		return 'https://github.com/lindseydiloreto/craft-languagelink';
+		//return 'http://doublesecretagency.com';
+	}
+
+	public function addTwigExtension()
+	{
+		Craft::import('plugins.languagelink.twigextensions.LanguageLinkTwigExtension');
+		return new LanguageLinkTwigExtension();
+	}
+
 }
